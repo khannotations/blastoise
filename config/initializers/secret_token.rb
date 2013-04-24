d356cd62ab51047792ec88c762bc0ab29b18ef13
Rafi Khan <faiaz.a.khan@gmail.com>
2013-04-24 16:34:18 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blastoise::Application.config.secret_token = '6655688e7470c2101a08cb4838f85e61af3bff63ee396d40b853580e6faac29eb4f3685ba683c74abe17303c956e1c22efba6e6d0ca34cca518906da6dfc0d49'
