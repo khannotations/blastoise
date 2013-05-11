dbcb6e7a7968192954694a51e4a88b684e8a1a21
Rafi Khan <faiaz.a.khan@gmail.com>
2013-05-11 04:45:23 -0700

    creds back in

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Blastoise::Application.initialize!
+
+# In production, set the environment variables directly.
+unless Rails.env.production?
+  credentials = YAML.load_file("#{Rails.root}/config/credentials.yml")
+  ENV['r_query'] = credentials['r_query']
+end
